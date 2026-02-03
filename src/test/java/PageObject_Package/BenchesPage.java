70a4a108a390450a9221ea6e5e44abf37486eed8
Barman <2458012@cognizant.com>
2026-02-03 09:20:59 +0530

    Userst-10

--- src/test/java/PageObject_Package/BenchesPage.java
+++ src/test/java/PageObject_Package/BenchesPage.java
@@ -20,26 +20,45 @@ public class BenchesPage {
     }
 
     public void openMetalBenches() {
-         try {
-            WebElement clibenches = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"clip-heder-desktop\"]/div/owl-carousel-o/div/div[1]/owl-stage/div/div/div[3]/pf-clip-category-list/div/a/div/div[1]")));
+        try {
+            WebElement clibenches = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(
+                    "//*[@id=\"clip-heder-desktop\"]/div/owl-carousel-o/div/div[1]/owl-stage/div/div/div[3]/pf-clip-category-list/div/a/div/div[1]")));
             clibenches.click();
-        } catch (Exception ignored) {}
-        
+        } catch (Exception ignored) {
+        }
+
     }
 
     public void expandMaterialFilter() {
-       
+
     }
 
     public void expandPriceFilter() {
-       
+
     }
 
     public void setMinPriceAndApply() {
-       
+
     }
 
-    public String topNItemsWithPrice() {
-       
+    public String topNItemsWithPrice(int n) {
+        try {
+            WebElement any = driver.findElement(
+                    By.xpath("//*[@class='product-name color-tertiary text-md font-medium ng-star-inserted']"));
+            actions.scrollToElement(any).perform();
+            List<WebElement> names = driver.findElements(
+                    By.xpath("//*[@class='product-name color-tertiary text-md font-medium ng-star-inserted']"));
+            List<WebElement> prices = driver
+                    .findElements(By.xpath("//*[@class='product-offer-price font-bold text-xl ng-star-inserted']"));
+            int size = Math.min(n, Math.min(names.size(), prices.size()));
+            String[][] arr = new String[size][2];
+            for (int i = 0; i < size; i++) {
+                arr[i][0] = names.get(i).getText();
+                arr[i][1] = prices.get(i).getText();
+            }
+            return arr;
+        } catch (Exception e) {
+            return new String[0][0];
+        }
     }
 }
